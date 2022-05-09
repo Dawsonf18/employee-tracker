de65c5941714e6f5cd5444da38c127e5bc6f3f66
Dawson Filion <Dawsonfilion21@gmail.com>
2022-05-09 08:19:48 -0400

    values for roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,4 +7,13 @@ VALUES
 ('Security'),
 ('Human Resources')
 ('Finance'),
-('Information Systems and Tech')
+('Information Systems and Tech');
+
+INSERT INTO roles (title, salary, department_id,) 
+VALUES 
+('Web Developer', 90000, 1)
+('Engineer', 90000, 2)
+('Accountant', 70000, 3)
+('Manager', 70000, 4)
+('Sales Rep', 40000, 5)
+('Paralegal', 50000, 6);
