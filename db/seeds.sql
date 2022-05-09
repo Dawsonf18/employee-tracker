bde7c8547e4eab4fd78d11b861c06bf5813655c1
Dawson Filion <Dawsonfilion21@gmail.com>
2022-05-09 08:30:21 -0400

    fixed values for employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,4 +18,12 @@ VALUES
 ('Sales Rep', 40000, 5)
 ('Paralegal', 50000, 6);
 
-
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+('John Smith')
+('Malia Brown')
+('Sarah Lourd')
+('Tom Allen')
+('Sam Kash')
+('John Doe')
+('Mike Chan')
+('Ashley Rodriguez');
